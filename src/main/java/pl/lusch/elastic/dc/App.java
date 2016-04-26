93651b11c4045ed9f3e43a98f8670267710b9232
Łukasz Schabek <lukasz.schabek@gmail.com>
2016-04-26 20:23:49 +0200

    init

---
+++ src/main/java/pl/lusch/elastic/dc/App.java
@@ -0,0 +1,13 @@
+package pl.lusch.elastic.dc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
