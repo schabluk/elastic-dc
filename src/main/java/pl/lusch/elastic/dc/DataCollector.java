07c1c8ad0fd1fdc3a188ad4aa411d1ab912e59d0
Łukasz Schabek <lukasz.schabek@gmail.com>
2016-04-28 20:53:57 +0200

    load stats module

--- src/main/java/pl/lusch/elastic/dc/DataCollector.java
+++ src/main/java/pl/lusch/elastic/dc/DataCollector.java
@@ -1,13 +1,16 @@
 package pl.lusch.elastic.dc;
 
+import java.util.Collection;
+import java.util.Collections;
+
+import org.elasticsearch.common.inject.Module;
 import org.elasticsearch.plugins.Plugin;
 
-/**
- * Hello world!
- *
- */
+import pl.lusch.elastic.dc.modules.StatsModule;
+
 public class DataCollector extends Plugin
 {
+
 	@Override
 	public String description() {
 		return "Data Collector for ES";
@@ -17,4 +20,14 @@ public class DataCollector extends Plugin
 	public String name() {
 		return "elastic-dc";
 	}
+
+	@Override
+	public Collection<Module> nodeModules() {
+		
+		return Collections.<Module>singletonList(
+			new StatsModule()
+		);
+		
+	}
+
 }
